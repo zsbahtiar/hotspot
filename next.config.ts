6278145ac80053635e4bc741af5b1818414b9ef1
Nechita <nechitasamantha@gmail.com>
2025-10-06 20:56:36 +0700

    change

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,7 @@ export const hotspotApiUrl = '/api/hotspot'
 export const queryApiUrl = '/api/query'
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/hotspot/:path*',
-        destination: 'https://backend.olaphotspot.web.id/api/hotspot/:path*', 
-      },
-      {
-        source: '/api/query/:path*',
-        destination: 'https://backend.olaphotspot.web.id/api/query/:path*',
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
