0a64954c827c44301b954a45a731c4032b7143bb
Nechita <nechitasamantha@gmail.com>
2025-10-06 15:59:03 +0700

    Add API rewrites for hotspot and query endpoints

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+export const hotspotApiUrl = '/api/hotspot'
+export const queryApiUrl = '/api/query'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/hotspot/:path*',
+        destination: 'https://backend.olaphotspot.web.id/api/hotspot:path*', 
+      },
+      {
+        source: '/api/query/:path*',
+        destination: 'https://backend.olaphotspot.web.id/api/query/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
