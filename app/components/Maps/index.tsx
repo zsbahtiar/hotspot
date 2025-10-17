0920cdf703eee6c5fa0bbc7b7668b212bcbeed11
zam <zamkorong12@gmail.com>
2025-10-17 10:52:25 +0700

    refactor: move to app dir

--- app/components/Maps/index.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-
-const MapComponent = dynamic(() => import("./MapComponent"), { 
-  ssr: false,
-  loading: () => null
-});
-
-export default MapComponent;
--- app/environments/environment.development.tsx
+++
@@ -1,8 +0,0 @@
-// export const environment = {
-//     api: 'http://localhost:8080/api',
-//   };
-
-export const environment = {
-    api: 'https://backend-olap-hotspot-production-4e4a.up.railway.app',
-  };
-  
